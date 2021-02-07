e20caa729736b437293453bc502e5e346e2d2ea4
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2021-02-07 23:31:25 +0500

    Finish Project

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -30,7 +30,7 @@ const HomeScreen = ({ match }) => {
             {!keyword ? (
                 <ProductCarousel />
             ) : (
-                <Link to='/' className='btn btn-light'>
+                <Link to='/' className='btn btn-dark'>
                     Go Back
                 </Link>
             )}
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -89,7 +89,7 @@ const ProductEditScreen = ({ match, history }) => {
 
     return (
         <>
-            <Link to='/admin/productlist' className='btn btn-light my-3'>
+            <Link to='/admin/productlist' className='btn btn-dark my-3'>
                 Go Back
             </Link>
             <FormContainer>
