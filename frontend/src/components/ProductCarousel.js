27654dbbd059ca982b504b7c27bf65bc76831b14
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2021-10-03 16:52:13 +0500

    changed font

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -25,7 +25,7 @@ const ProductCarousel = () => {
             {products.map(product => (
                 <Carousel.Item key={product._id}>
                     <Link to={`/product/${product._id}`}>
-                        <Image src={product.image} alt={product.name} fluid />
+                        <Image src={product.image} alt={product.name} fluid className='fs-sm-10' />
                         <Carousel.Caption className='carousel-caption'>
                             <h2>
                                 {product.name} (${product.price})
